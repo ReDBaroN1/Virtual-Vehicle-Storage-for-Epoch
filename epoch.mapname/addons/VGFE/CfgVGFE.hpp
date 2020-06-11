f336cd38b738b9e74153a175e553b52cfc10f128
Chris Cardozo <cardocp@hotmail.com>
2020-06-11 07:21:16 -0400

    Client files v0.1

---
+++ epoch.mapname/addons/VGFE/CfgVGFE.hpp
@@ -0,0 +1,10 @@
+
+#include "VGFE_dialogs.hpp"
+class CfgVGFE {
+	useDynamenu = 0;  // set to 1 to use the dynamenu to access specific vehicles, or 0 to open a dialog with vehicles in VG and locally listed.
+	requirePlotpolNearby = 1;
+	vehicleSearchDistance = 50;
+	vgfeExpires = 60 * 60 * 24 * 30;  // 30 days
+	virtualHangerObject = "Land_JetEngineStarter_01_F";
+	virtualDockObject = "Land_Destroyer_01_Boat_Rack_01_F";
+};
