6abd30cf7a3e79d1f40970d72b6e4ce1227ff8b2
Chris Cardozo <cardocp@hotmail.com>
2020-06-18 12:15:53 -0400

    Fixed Error in CfgRemoteExec
    Thank you He-man for finding this.

--- epoch.mapname/addons/VGFE/CfgRemoteExecVGFE.hpp
+++ epoch.mapname/addons/VGFE/CfgRemoteExecVGFE.hpp
@@ -1,8 +1,8 @@
 /*
 	Merge this with \epoch_config\Configs\CfgRemoteExec.hpp
 */
-class VGFE_fnc_client_sendVGFERequest 
-{
-	allowedTargets = 2; 
-	jip = 0;
-};
+        class VGFE_fnc_handleClientRequest 
+        {
+            allowedTargets = 2; 
+            jip = 0;
+        };
