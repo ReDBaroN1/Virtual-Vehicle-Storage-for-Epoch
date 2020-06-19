21142b4e12748fbc6285cafe4faccc391b7f5462
BaroN <baronepoch@hotmail.com>
2020-06-19 18:48:00 +0100

    removed VGFE_fnc_client_sendVGFERequest
    As discussed, removed the VGFE_fnc_client_sendVGFERequest block

--- epoch.mapname/epoch_config/Configs/CfgRemoteExec.hpp
+++ epoch.mapname/epoch_config/Configs/CfgRemoteExec.hpp
@@ -1,11 +1,6 @@
 /*
 	Merge this with \epoch_config\Configs\CfgRemoteExec.hpp
 */
-		class VGFE_fnc_client_sendVGFERequest
-		{
-			allowedTargets = 2;
-			jip = 0;
-		};
 		class vgfe_fnc_handleclientrequest
 		{
 			allowedTargets = 2;
